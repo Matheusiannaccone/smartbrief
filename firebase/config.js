dea3725f8fc6088890d521c5289a24f732e1d02e
Matheus Iannaccone <matheusiannaccone@outlook.com>
2026-05-17 13:27:47 -0300

    Remove Firebase config from config.js
    Removed sensitive Firebase configuration details.

--- firebase/config.js
+++ firebase/config.js
@@ -4,12 +4,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbGItRydBzn_jdj_HmvHqeRssih7E5XUU",
-  authDomain: "smart-brief-84c45.firebaseapp.com",
-  projectId: "smart-brief-84c45",
-  storageBucket: "smart-brief-84c45.firebasestorage.app",
-  messagingSenderId: "1049988824096",
-  appId: "1:1049988824096:web:6e6b25a686fd2fbc4dd045"
+
 };
 
 // Initialize Firebase
